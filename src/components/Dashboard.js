0b05933e47eea6d83b6a1812f29b3b3542e53691
Phil Schilter <phil.schilter@gmail.com>
2017-03-30 14:52:36 +0200

    :sparkles: show user accountNr and get transactions

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,9 @@
 
 import React from 'react'
 
+import { getTransactions } from '../api'
+import { User, Transaction } from '../api'
+
 /*
   Use the api functions to call the API server. For example, the transactions
   can be retrieved and stored in the state as follows:
@@ -16,16 +19,26 @@ import React from 'react'
 
 export type Props = {
   token: string,
+  user: ?User,
 }
 
 class Dashboard extends React.Component {
 
-  props: Props
+  state: {
+    user: ?User,
+    transactions: Array<Transaction>,
+  }
+
+  componentDidMount() {
+    getTransactions(this.props.token)
+      .then(({ result: transactions }) => this.setState({ transactions }));
+  }
 
   render() {
 
     return (
       <div>
+        <h1>{this.props.user.accountNr}</h1>
         This is the dashboard
       </div>
     )
