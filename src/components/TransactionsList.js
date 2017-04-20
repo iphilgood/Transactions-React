28c471310f95cf4ca52347826682094a1687d0eb
Roman Blum <rmnblm@gmail.com>
2017-04-20 16:29:35 +0200

    :lipstick: format dates

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,12 +1,13 @@
 // @flow
 
 import React from 'react';
+import Moment from 'react-moment';
 
 function TransactionsList({transactions}) {
 
     const renderTransactionRow = ({date, from, target, amount, total}, index) =>
       <tr key={index}>
-        <td>{date}</td>
+        <td><Moment format="DD.MM.YYYY">{date}</Moment></td>
         <td>{from}</td>
         <td>{target}</td>
         <td>{amount} CHF</td>
