1d83514c9efa09fadc926c2f829b86b363d83266
Roman Blum <rmnblm@gmail.com>
2017-04-20 16:29:25 +0200

    1 hotfix für den 1 boy

--- src/components/AllTransactions.js
+++ src/components/AllTransactions.js
@@ -56,7 +56,7 @@ class AllTransactions extends React.Component {
 
   updateTransactions = (year, month) => {
     const firstDay = new Date(year, month, 1);
-    const lastDay = new Date(year, month + 1, 0);
+    const lastDay = new Date(year, parseInt(month, 10) + 1, 0);
     getTransactionsBetween(firstDay, lastDay, this.props.token)
         .then(({ result: transactions }) => this.setState({ transactions }));
   }
